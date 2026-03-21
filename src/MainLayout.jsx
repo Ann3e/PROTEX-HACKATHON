15a9abccece0d5e5c947165f16378ff906919f7d
archangel2006 <vaibhavideep26@gmail.com>
2026-03-21 21:11:07 +0530

    Add Timeline

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -6,7 +6,7 @@ import Header from "./sections/Header/Header";
 import About from "./sections/About/About";
 import Hero from "./sections/Hero/Hero";
 import Tracks from "./sections/Tracks/Tracks";
-// import Timeline from "./sections/Timeline/Timeline";
+import Timeline from "./sections/Timeline/Timeline";
 // import Registration from "./sections/Registration/Registration";
 // import Prizes from "./sections/Prizes/Prizes";
 import FAQ from "./sections/FAQ/FAQ";
@@ -24,12 +24,13 @@ export default function MainLayout() {
       <Hero />
       <About />
       <Tracks />
+      <Timeline />
       <Eligibility />
       <FAQ />
       <Footer /> 
 
   
-      {/*<Timeline />
+      {/*
       <Registration />
       <Prizes />   */}
     
